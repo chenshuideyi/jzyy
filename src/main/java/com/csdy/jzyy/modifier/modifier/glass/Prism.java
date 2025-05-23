e5a5f9a5da454a191ae6fdc9116b7ef96562eb12
CSDY <204125@qq.com>
2025-05-23 18:34:14 +0800

    wait

---
+++ src/main/java/com/csdy/jzyy/modifier/modifier/glass/Prism.java
@@ -0,0 +1,45 @@
+package com.csdy.jzyy.modifier.modifier.glass;
+
+import com.c2h6s.etstlib.register.EtSTLibHooks;
+import com.c2h6s.etstlib.tool.hooks.ArrowHitModifierHook;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.ModifierHooks;
+import slimeknights.tconstruct.library.modifiers.hook.combat.MeleeHitModifierHook;
+import slimeknights.tconstruct.library.modifiers.hook.ranged.ProjectileHitModifierHook;
+import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
+import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+import slimeknights.tconstruct.library.tools.nbt.ModDataNBT;
+import slimeknights.tconstruct.library.tools.nbt.ModifierNBT;
+
+import javax.annotation.Nullable;
+
+import static com.csdy.jzyy.modifier.util.CsdyModifierUtil.isEntityInSunlight;
+
+public class Prism extends NoLevelsModifier implements MeleeHitModifierHook, ArrowHitModifierHook {
+
+    @Override
+    public void afterMeleeHit(IToolStackView tool, ModifierEntry modifier, ToolAttackContext context, float damageDealt) {
+        var attacker = context.getAttacker();
+        var target= context.getLivingTarget();
+        if (target == null) return;
+        if (isEntityInSunlight(attacker)) target.setSecondsOnFire(10);
+    }
+
+    @Override
+    public void afterArrowHit(ModDataNBT persistentData, ModifierEntry entry, ModifierNBT modifiers, AbstractArrow arrow, @Nullable LivingEntity attacker, @NotNull LivingEntity target, float damageDealt) {
+        if (attacker != null && isEntityInSunlight(attacker)) target.setSecondsOnFire(10);
+    }
+
+    @Override
+    protected void registerHooks(ModuleHookMap.Builder hookBuilder) {
+        hookBuilder.addHook(this, ModifierHooks.MELEE_HIT);
+        hookBuilder.addHook(this, EtSTLibHooks.ARROW_HIT);
+    }
+}
