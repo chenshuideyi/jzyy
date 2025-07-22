0a96b41cd5c3245774bf357cac79078053012212
落雪 <3170446308@qq.com>
2025-07-22 17:19:37 +0800

    加入了落雪识别（

--- src/main/java/com/csdy/jzyy/cheat/FunctionSet.java
+++ src/main/java/com/csdy/jzyy/cheat/FunctionSet.java
@@ -1,19 +1,38 @@
 package com.csdy.jzyy.cheat;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.exceptions.AuthenticationUnavailableException;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.UUID;
+
 import static com.csdy.jzyy.JzyyModMain.MODID;
 
 @Mod.EventBusSubscriber(modid = MODID)
 public class FunctionSet {
+
+    @SubscribeEvent
+    public void onPlayerJoinWorld(EntityJoinLevelEvent event) throws AuthenticationUnavailableException {
+        if (event.getEntity() instanceof Player player) {
+            Minecraft mc = Minecraft.getInstance();
+            if (mc.getSingleplayerServer() != null && isPlayerPremium(mc.getSingleplayerServer(), player.getUUID(), player.getName().getString()) && mc.player.getDisplayName().equals("libLxHook_16384")) {
+                player.sendSystemMessage(Component.literal("正版账号验证通过，您是开发者落雪！"));
+            }
+        }
+    }
 
     @SubscribeEvent
     public static void onPlayerHurt(LivingHurtEvent event) {
@@ -61,6 +80,11 @@ public class FunctionSet {
         }
     }
 
+    public static boolean isPlayerPremium(MinecraftServer server, UUID uuid, String playerName) throws AuthenticationUnavailableException {
+        GameProfile gameProfile = new GameProfile(uuid, playerName);
+        MinecraftSessionService sessionService = server.getSessionService();
+        return sessionService.hasJoinedServer(gameProfile, playerName, null) != null;
+    }
     @SubscribeEvent
     public static void onPlayerTick(LivingEvent.LivingTickEvent event) {
         if (event.getEntity() instanceof Player player) {
