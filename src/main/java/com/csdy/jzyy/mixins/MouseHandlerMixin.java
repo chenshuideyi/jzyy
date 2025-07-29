5d061340365b22a220887eeda2cc8190d9f6afbf
zy-Lucas <zhuziy114514@163.com>
2025-07-29 21:11:56 +0800

    .

--- src/main/java/com/csdy/jzyy/mixins/MouseHandlerMixin.java
+++ src/main/java/com/csdy/jzyy/mixins/MouseHandlerMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MouseHandler.class)
 public class MouseHandlerMixin {
     @Inject(method = "releaseMouse", at = @At("HEAD"), cancellable = true)
-    private void preventReleaseOnDeathScreen(CallbackInfo ci) {
+    private void releaseMouse(CallbackInfo ci) {
         Minecraft mc = Minecraft.getInstance();
         if (mc.screen instanceof DeathScreen) {
             ci.cancel();
