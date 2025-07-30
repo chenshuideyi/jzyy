e2f89eea96d1d0eb0fcee758cb83be26058e893b
CSDY <204125@qq.com>
2025-07-30 11:20:45 +0800

    回退一下调灵斯拉，今天补寄生虫材料，再给csdy加个召唤物并加强

--- src/main/java/com/csdy/jzyy/mixins/KeyMappingMixin.java
+++ src/main/java/com/csdy/jzyy/mixins/KeyMappingMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeyMapping.class)
 public class KeyMappingMixin {
-    @Inject(method = "releaseAll", at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "releaseAll", at = @At("HEAD"), cancellable = true)
     private static void onReleaseAll(CallbackInfo ci) {
         Minecraft mc = Minecraft.getInstance();
         if (mc.screen instanceof DeathScreen) {
