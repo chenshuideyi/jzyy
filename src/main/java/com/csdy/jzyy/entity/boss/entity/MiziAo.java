823c5c7215327060abb84d7c171ce90fead88dff
zy-Lucas <zhuziy114514@163.com>
2025-08-04 14:21:01 +0800

    。

--- src/main/java/com/csdy/jzyy/entity/boss/entity/MiziAo.java
+++ src/main/java/com/csdy/jzyy/entity/boss/entity/MiziAo.java
@@ -349,8 +349,12 @@ public class MiziAo extends BossEntity implements GeoEntity {
 
 
     public static AttributeSupplier.Builder createAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MOVEMENT_SPEED, 0.3).add(Attributes.MAX_HEALTH, RATING)
-                .add(Attributes.ARMOR, RATING).add(Attributes.ATTACK_DAMAGE, PERFECT).add(Attributes.ATTACK_SPEED, 1.0)
+        return Mob.createMobAttributes()
+                .add(Attributes.MOVEMENT_SPEED, 0.3)
+                .add(Attributes.MAX_HEALTH, RATING)
+                .add(Attributes.ARMOR, RATING)
+                .add(Attributes.ATTACK_DAMAGE, PERFECT)
+                .add(Attributes.ATTACK_SPEED, 1.0)
                 .add(Attributes.FOLLOW_RANGE, 32.0);
     }
 
