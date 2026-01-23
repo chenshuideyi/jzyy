a998f1ea2e99192aace52e657743648d8d4826d6
霧雨烨 <Web_13234@outlook.com>
2026-01-23 23:24:33 +0800

    何意味？

--- src/main/java/com/csdy/jzyy/mixins/MinecraftMixin.java
+++ src/main/java/com/csdy/jzyy/mixins/MinecraftMixin.java
@@ -11,23 +11,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
 
-    @Shadow
-    public static int fps;
+//    @Shadow
+//    public static int fps;
 
-    @Inject(method = "runTick", at = @At("TAIL"))
-    private void onRunTick(boolean render, CallbackInfo ci) {
-        int realFps = fps;
-        fps = realFps * 5;
-    }
+//    @Inject(method = "runTick", at = @At("TAIL"))
+//    private void onRunTick(boolean render, CallbackInfo ci) {
+//        int realFps = fps;
+//        fps = realFps * 5;
+//    }
 
     //显然上面的更好玩
 
-//    @Redirect(
-//            method = "runTick",
-//            at = @At(value = "FIELD", target = "Lnet/minecraft/client/Minecraft;fps:I", opcode = 179)
-//    )
-//    private void onSetFps(int newValue) {
-//        Minecraft.fps = newValue * 255;
-//    }
+    // 但是显然上面的很奇异搞笑，是该做成只有csdy出场时候才生效
+
+    @Redirect(
+            method = "runTick",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/client/Minecraft;fps:I", opcode = 179)
+    )
+    private void onSetFps(int newValue) {
+        Minecraft.fps = newValue * 255;
+    }
 
 }
